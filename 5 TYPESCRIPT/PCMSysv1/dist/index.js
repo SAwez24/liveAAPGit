e2a62fd020d2e66755892283c20f6932771e9ffd
DEV <dev@dev.com>
2022-05-20 15:41:02 +0530

    commit..

--- 5 TYPESCRIPT/PCMSysv1/dist/index.js
+++ 5 TYPESCRIPT/PCMSysv1/dist/index.js
@@ -1,7 +1,33 @@
 // plain old javascript
+var x;
 var x = 10;
 console.log(x);
 x = 1.05;
 console.log(x);
-x = "welcome";
+// Type 'string' is not assignable to type 'number'
+// x = "hello";
+var y;
+y = "welcome";
 console.log(x);
+// let keyword
+let z = 200;
+let msg = "let keyword";
+console.log(z);
+console.log(msg);
+const productTS = {
+    productId: "P01",
+    price: 500,
+    productName: "TS"
+};
+console.log(productTS);
+// class
+class ProductClass {
+    constructor(productId, price, productName) {
+        this.productId = productId;
+        this.price = price;
+        this.productName = productName;
+    }
+}
+// create product
+var productObject = new ProductClass("P1", 50, "Book");
+console.log(productObject);
--- 5 TYPESCRIPT/PCMSysv1/src/index.ts
+++ 5 TYPESCRIPT/PCMSysv1/src/index.ts
@@ -1,7 +1,44 @@
 // plain old javascript
+var x: number;
 var x = 10;
 console.log(x);
 x = 1.05;
 console.log(x);
-x = "welcome";
+// Type 'string' is not assignable to type 'number'
+// x = "hello";
+var y : string;
+y = "welcome";
 console.log(x);
+
+// let keyword
+let z = 200;
+let msg = "let keyword"
+console.log(z)
+console.log(msg)
+
+interface Product {
+   productId: string;
+   price : number;
+   productName : string;
+}
+const productTS : Product =  {
+   productId: "P01",
+   price : 500,
+   productName : "TS"
+}
+console.log(productTS);
+
+// class
+class ProductClass {
+   productId: string;
+   price : number;
+   productName : string;
+   constructor( productId: string, price : number,productName : string) {
+      this.productId = productId;
+      this.price = price;
+      this.productName = productName;
+   }
+}
+// create product
+var productObject = new ProductClass("P1",50,"Book");
+console.log(productObject);
