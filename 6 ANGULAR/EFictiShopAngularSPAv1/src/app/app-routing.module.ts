481722cba2d90a7c2163c44bdb7414e1f4a032e5
DEV <dev@dev.com>
2022-06-01 16:52:02 +0530

    commit..

--- 6 ANGULAR/EFictiShopAngularSPAv1/src/app/app-routing.module.ts
+++ 6 ANGULAR/EFictiShopAngularSPAv1/src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { AddEmpComponent } from "./add-emp/add-emp.component";
 import { ListEmpComponent } from "./list-emp/list-emp.component";
 import { LoginComponent } from "./login/login.component";
+import { AuthGuardService } from "./service/auth-guard.service";
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
-    { path: 'list-emp', component: ListEmpComponent },
-    {path: '**', component: LoginComponent},
+    { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'list-emp', component: ListEmpComponent, canActivate: [AuthGuardService] },
+    { path: 'add-emp', component: AddEmpComponent, canActivate: [AuthGuardService]  },
+    { path: '**', component: LoginComponent },
 ]
 @NgModule({
     imports: [CommonModule, RouterModule.forRoot(routes)],
--- 6 ANGULAR/EFictiShopAngularSPAv1/src/app/login/login.component.ts
+++ 6 ANGULAR/EFictiShopAngularSPAv1/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,33 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginFormGroup : FormGroup;
-  constructor(private formBuilder : FormBuilder) { }
+  constructor(private formBuilder : FormBuilder, private router : Router) { }
 
   ngOnInit(): void {
     this.loginFormGroup =  this.formBuilder.group ( {
-      loginId : [],
-      password : []
-      // loginId : ["teddy"],
-      // password : ["bear"]
+      // loginId : [''],
+      // password : []
+      loginId : ["teddy"],
+      password : ["bear"]
     });
   }
+ onSubmit() {
+   console.log("logging.")
+   console.log(this.loginFormGroup.value)
+   console.log(this.loginFormGroup.controls['loginId'].value);
+   console.log(this.loginFormGroup.controls['password'].value);
 
+   const loginId : string = this.loginFormGroup.controls['loginId'].value;
+   const password : string = this.loginFormGroup.controls['password'].value;
+
+   if(loginId == 'teddy' && password == 'bear') {
+   this.router.navigate(['list-emp'])
+   sessionStorage.setItem('loggedIn','yes')
+   }
+   else
+   {
+    this.router.navigate(['login'])
+    sessionStorage.setItem('loggedIn','no')
+   }
+ }
 }
