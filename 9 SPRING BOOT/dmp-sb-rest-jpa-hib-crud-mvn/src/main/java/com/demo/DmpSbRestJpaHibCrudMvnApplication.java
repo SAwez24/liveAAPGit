2479fcffc3a0004a9a83c21914750ff03a232a89
DEV <dev@dev.com>
2022-06-16 15:38:17 +0530

    commit..

---
+++ 9 SPRING BOOT/dmp-sb-rest-jpa-hib-crud-mvn/src/main/java/com/demo/DmpSbRestJpaHibCrudMvnApplication.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmpSbRestJpaHibCrudMvnApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Java App...");
+		System.out.println("Loading Spring & SpringBoot Framework....");
+		SpringApplication.run(DmpSbRestJpaHibCrudMvnApplication.class, args);
+	}
+
+}
